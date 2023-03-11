23b78e23c1d57ec7efcb0189dd09a276c9cfbd51
MohamadMehdiDabestani <Mohamad1382mhd@gmail.com>
2023-03-10 21:37:00 -0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-
+import {Testing} from '@api/shared'
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  const d : Testing = {title : "this is from api"}
+  res.send({ message: d.title });
 });
 
 app.listen(port, host, () => {
